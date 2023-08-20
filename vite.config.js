69f321760191d1f11f99dad50a588f4268bbf4fb
Loc Phuoc Dang <locphuocdang@Locs-MBP.home>
2023-08-20 23:40:34 +0300

    second  commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/bookstorefirebasereact/', // add this property and the value is the name of your repository
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  } 
 })
